7ba1ed3bb02805cbd37f34ef6fd53d934e557ac1
DooYee <149545221+DOOYEE0709@users.noreply.github.com>
2025-08-06 16:38:22 +0900

    Revert "[FEAT] 출석 component"

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,5 +1,4 @@
 export const API_BASE = '/api/v1';
 
 export const USER_BASE = `${API_BASE}/users`;
-export const PET_BASE = `${API_BASE}/pets`;
-export const EVENT_BASE = `${API_BASE}/events`;
+export const PET_BASE = `${API_BASE}/pets`
